f21c0b886d1f13e3c2db3fb85d9db69da4934832
Noah Gordon <tortuga.ng@gmail.com>
2024-11-24 18:39:30 -0500

    Access Rails env in client

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,14 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+
+// third party imports
 import "@hotwired/turbo-rails"
-import "controllers"
 import * as bootstrap from "bootstrap"
+
+if (window._RAILS_ENV == null) {
+    console.error('application.js initializing before Rails env is set! Something is wrong.');
+} else if (window._RAILS_ENV !== 'production') {
+    console.log(`Initializing applications.js in ${window._RAILS_ENV}`);
+}
+
+// our imports
+import "controllers"
